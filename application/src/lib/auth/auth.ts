87e7168457b4612b23d22a35dd9bae68cc8cb0e0
shrvansudhakara <shrvansudhakara@gmail.com>
2026-03-01 16:57:29 +0530

    feat: add Google, X (Twitter) OAuth authentication with login modal

---
+++ application/src/lib/auth/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    twitter: {
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+    },
+  },
+  plugins: [nextCookies()],
+});
